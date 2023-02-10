b79c0b01b50bbbc62587e943f65a8492794f91c5
Thom-Sandbrink <497928@student.saxion.nl>
2023-02-10 12:10:20 +0100

    Programm done

--- main.py
+++ main.py
@@ -11,26 +11,47 @@ file_types = [
 ]
 
 layout = [
-    [sg.Image(key="-IMAGE-")],
-    [sg.Text(key="-FILENAME-")],
-    [sg.Image(key="-IMAGEREMOVED-")],
-
     [
         sg.Text("Image File"),
         sg.Input(size=(25, 1), key="-FILE-"),
         sg.FileBrowse(file_types=file_types),
-        sg.Button("Load Image")
-    ]
+        sg.Button("Convert"),
+    ],
+
+    [
+        sg.Text("New image location"),
+        sg.Input(size=(25, 1), key="-NEWLOCATION-"),
+        sg.FolderBrowse(),
+    ],
+
+    [
+        sg.Text("Make sure filename.png is behind the folder location."),
+    ],
+
+    [
+        sg.Text("Default location at ouput/ouput.png")
+    ],
+
+    [
+        sg.Image(key="-IMAGE-"),
+        sg.Image(key="-IMAGEREMOVED-")
+    ],
+
+    [sg.Text(key="-FILENAME-")],
+    [sg.Text(key="-NEWLOCATIONNAME-")],
+
 ]
 
-window = sg.Window("Image viewer", layout)
+window = sg.Window("Image background remover", layout, size=(900, 450))
 
 while True:
     event, values = window.read()
     if event == "Exit" or event == sg.WIN_CLOSED:
         break
-    if event == "Load Image":
+
+    if event == "Convert":
         filename = values["-FILE-"]
+        foldername = values["-NEWLOCATION-"]
         if os.path.exists(filename):
             image = Image.open(values["-FILE-"])
             image.thumbnail((400, 400))
@@ -38,10 +59,19 @@ while True:
             image.save(bio, format="PNG")
             window["-IMAGE-"].update(data=bio.getvalue())
 
-            window["-FILENAME-"].update(filename)
+            window["-FILENAME-"].update("Input path" + filename)
 
             input_path = Path(filename)
-            output_path = 'output/output.png'
+
+            if foldername != "":
+                if not (".png" or ".PNG") in foldername:
+                    output_path = foldername + "/newImage.png"
+                else:
+                    output_path = foldername
+
+            else:
+                output_path = "output/output.png"
+
             inputForProgram = Image.open(input_path)
             output = remove(inputForProgram)
             output.save(output_path)
@@ -51,5 +81,6 @@ while True:
             bioOutput = io.BytesIO()
             imageOutput.save(bioOutput, format="PNG")
             window["-IMAGEREMOVED-"].update(bioOutput.getvalue())
+            window["-NEWLOCATIONNAME-"].update("Output path: " + output_path)
 
 window.close()
